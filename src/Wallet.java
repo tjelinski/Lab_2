5f4efc223d803eb465c1fdf02f38871b4ece3915
t.jelinski <tjelinski@student.wsb-nlu.edu.pl>
2023-11-05 14:25:21 +0100

    add comments

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,43 +1,44 @@
 public class Wallet {
-    private double howMuchMoney;
-    private String currency;
+    private double howMuchMoney; // liczba reprezentuje ilość portfela tzn. ilość srodków [cecha]
+    private String currency; // waluta w jakiej przechowywane sa środki w portfelu [cecha]
 
-    public Wallet(double howMuchMoney, String currency) {
+    public Wallet(double howMuchMoney, String currency) {   // konstruktor klasy wallet wywoływany przy tworzeniu nowego obiektu przyjmuje dwa argumenty howMuchMoney oraz currency
         this.howMuchMoney = howMuchMoney;
         this.currency = currency;
     }
 
-    public void addMoney(double amount) {
+    public void addMoney(double amount) { // metoda do dodawania pieniędzy do portfela
         howMuchMoney += amount;
     }
 
-    public void subtractMoney(double amount) {
+    public void subtractMoney(double amount) { // metoda do odejmowania pieniedzy z portfela
         if (howMuchMoney >= amount) {
             howMuchMoney -= amount;
         } else {
-            System.out.println("You do not have enough money");
+            System.out.println("You do not have enough money, change the amount to be paid out");
         }
     }
 
-    public double getBalance() {
+    public double getBalance() { // metoda zwracajaca saldo portfela
+        System.out.println("Free funds: ");
         return howMuchMoney;
     }
 
-    public String getCurrency() {
+    public String getCurrency() { //metoda zwracajaca walutę środków
         return currency;
     }
 
-    public void newCurrency(String newCurrency) {
+    public void newCurrency(String newCurrency) { // metoda pozwala na zmine waluty w portfelu
         currency = newCurrency;
     }
 
     public static void main(String[] args) {
-        Wallet myWallet = new Wallet(0.0, "USD");
-        myWallet.addMoney(50.5);
-        myWallet.subtractMoney(32.7);
-        System.out.println("Your current balance" + myWallet.getBalance() + myWallet.getCurrency());
-        myWallet.subtractMoney(40);
-        System.out.println(myWallet.getBalance());
+        Wallet myWallet = new Wallet(0.0, " USD"); //tworzenie nowego obiektu z 0.00 USD
+        myWallet.addMoney(50.5); // dodanie 50.5 USD
+        myWallet.subtractMoney(32.7); // odjęcie  32.7 USD
+        System.out.println(myWallet.getBalance() + myWallet.getCurrency()); // wyświetlenie salda w atulanej walucie
+        myWallet.subtractMoney(40); // odjecie 40 USD
+        System.out.println(myWallet.getBalance()); //
     }
 }
 
